baeafaf881ebb62fa2e3264c583591563cb21bb6
raresql <121085931+raresql@users.noreply.github.com>
2023-01-03 12:54:35 +0500

    Create How to Refresh SSMS IntelliSence Local Cache.sql

---
+++ Solutions/How to Refresh SSMS IntelliSence Local Cache.sql
@@ -0,0 +1,14 @@
+--Script Name: How to Refresh SSMS IntelliSence Local Cache
+--Script Type: Solution
+--Article: https://raresql.com/2014/02/07/sql-server-how-to-refresh-ssms-intellisense-cache/
+--Developed By: Muhammad Imran
+--Date Created: 07 Feb 2014
+--Date Modified: 26 Dec 2022
+
+--Use database tempdb
+USE tempdb
+GO
+
+--Browse table
+Select * FROM tbl_employee
+GO
